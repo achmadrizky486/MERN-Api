4a218b2797abe5f36dab49e677123ddfb17504cc
achmadrizky486 <achmadrizky486@gmail.com>
2022-07-31 09:15:01 +0700

    feat : restucture projects

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 const express = require("express");
 const app = express();
-const router = express.Router();
+const productRoutes = require("./src/routes/products");
 
-router.use("/products", (req, res, next) => {
-  res.json({
-    name: "Achmad Rizky",
-    emai: "achmad@gmail.com",
-  });
-  next(); //next biar lanjut ke method yg lain jika ada
-}); //buat endpoint '/products' yg pny default parameter req, res, next
+app.use("/", productRoutes);
+app.listen(4000);
 
-router.use("/price", (req, res, next) => {
-  res.json({
-    price: 3000,
-  });
-  next();
-});
-router.delete("/cust", (req, res, next) => {
-  //Router spesifik
-  res.json({
-    title: "popoppopop",
-  });
-  next();
-});
+// const router = express.Router();
 
-app.use("/", router); //ketika di run, dia akan nyari sesuai router
-app.listen(4000); // Buat app server saya berjalan di port 4000. Lalu jalankan node index.js. buka localhost:4000 maka line 4 berjalan
+// router.use("/products", (req, res, next) => {
+//   res.json({
+//     name: "Achmad Rizky",
+//     emai: "achmad@gmail.com",
+//   });
+//   next(); //next biar lanjut ke method yg lain jika ada
+// }); //buat endpoint '/products' yg pny default parameter req, res, next
 
-// Lalu instal nodemon, npm install nodemon
-// skrg ngerun server tinggal npx nodemon index.js (pakai nodemon bisa auto reload)
-// Tambah   "start" : "nodemon index.js" di package.json. Jadi kalau ngerun tinggal npm start beda sprti line 9
-// git init -> git status -> buat .gitignore node_modules -> git remote add origin https://github.com/achmadrizky486/MERN-Api.git -> git add . -> commit seperti biasa, trus push sync
+// router.use("/price", (req, res, next) => {
+//   res.json({
+//     price: 3000,
+//   });
+//   next();
+// });
+// router.delete("/cust", (req, res, next) => {
+//   //Router spesifik
+//   res.json({
+//     title: "popoppopop",
+//   });
+//   next();
+// });
 
-//Buat router di line 3
+// app.use("/", productRoutes); //ketika di run, dia akan nyari sesuai router
+// app.listen(4000); // Buat app server saya berjalan di port 4000. Lalu jalankan node index.js. buka localhost:4000 maka line 4 berjalan
+
+// // Lalu instal nodemon, npm install nodemon
+// // skrg ngerun server tinggal npx nodemon index.js (pakai nodemon bisa auto reload)
+// // Tambah   "start" : "nodemon index.js" di package.json. Jadi kalau ngerun tinggal npm start beda sprti line 9
+// // git init -> git status -> buat .gitignore node_modules -> git remote add origin https://github.com/achmadrizky486/MERN-Api.git -> git add . -> commit seperti biasa, trus push sync
+
+// //Buat router di line 3
---
+++ src/controllers/products.js
@@ -0,0 +1,25 @@
+exports.createProduct = () => (req, res, next) => {
+  res.json({
+    message: "Create Product Succes",
+    data: {
+      id: 1,
+      name: "Masako Babi",
+      price: 1000,
+    },
+  });
+  next();
+};
+
+exports.getAllProducts = () => (req, res, next) => {
+  res.json({
+    message: "Get All Products Succes",
+    data: [
+      {
+        id: 1,
+        name: "Masako Babi",
+        price: 1000,
+      },
+    ],
+  });
+  next();
+};
