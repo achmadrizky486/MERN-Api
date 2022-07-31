ca061ddf8c6f7f9a6ae3ce88513d4bc44cb390cf
achmadrizky486 <achmadrizky486@gmail.com>
2022-07-31 21:06:43 +0700

    feat : add blog route & controller

---
+++ src/controllers/blog.js
@@ -0,0 +1,20 @@
+exports.createBlogPost = (req, res, next) => {
+  const title = req.body.title;
+  //   const image = req.body.image;
+  const body = req.body.body;
+  res.json({
+    message: "Create Blog Success",
+    data: {
+      post_id: 1,
+      title: "Title Blog",
+      //   image: "image.jpg",
+      body: "Lorem ipsum dolor si jamet",
+      created_at: "01/01/2022",
+      author: {
+        uid: 1,
+        name: "Apem",
+      },
+    },
+  });
+  res.status(201).json({ result });
+};
---
+++ src/routes/blog.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const blogController = require("../controllers/blog");
+
+//[POST] /v1/blog/post
+router.post("/post", blogController.createBlogPost);
+module.exports = router;
