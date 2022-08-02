f557426e08e06a0f45dcd9645fb8af5b41e3ce63
achmadrizky486 <achmadrizky486@gmail.com>
2022-08-02 22:50:29 +0700

    feat : Upload Image / Photo di NodeJS

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -2,21 +2,27 @@ const { validationResult } = require("express-validator"); //import
 const BlogPost = require("../models/blog");
 
 exports.createBlogPost = (req, res, next) => {
-  const title = req.body.title;
-  //   const image = req.body.image;
-  const body = req.body.body;
   const errors = validationResult(req); // buat variabel errors
-
   if (!errors.isEmpty()) {
     const err = new Error("Input Value Tidak Sesuai");
     errorStatus = 400;
     err.data = errors.array();
     throw err;
   }
+  if (!req.file) {
+    const err = new Error("Dimana images nyaaaaaa????? <Yusuf Mansur vibe>");
+    errorStatus = 422;
+    throw err;
+  }
+
+  const title = req.body.title;
+  const image = req.file.path;
+  const body = req.body.body;
 
   const Posting = new BlogPost({
     title: title,
     body: body,
+    image: image,
     author: {
       uid: 1,
       name: "Achmad",
